2216191e10d897e238783ec693a9134bce073822
loveryLove <itsdeslon@gmail.com>
2025-06-03 00:10:59 +0700

    Убрал панель и сделал ее снизу

--- app/src/main/java/com/example/apitestapps/MainActivity.kt
+++ app/src/main/java/com/example/apitestapps/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.apitestapps
 
 import android.R.attr.fontWeight
-import android.R.id.content
 import android.os.Bundle
 import android.view.Menu
 import androidx.activity.ComponentActivity
@@ -10,7 +9,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,22 +24,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DrawerDefaults.scrimColor
-import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -63,11 +64,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.apitestapps.ui.theme.APItestAppsTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -76,148 +74,158 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WindowMain(name: String, modifier: Modifier = Modifier) {
     val myFont = FontFamily(Font(R.font.porter, FontWeight.Light))
-    val items = listOf(" ","Домой", "Профиль", "Настройки")
-    val selectedItem = remember { mutableStateOf(items[0]) }
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
     var isConnected by remember { mutableStateOf(false) }
+    var isSheetOpen by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
 
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet(drawerContainerColor = Color.DarkGray) {
-                items.forEach { item ->
-                    TextButton(
-                        onClick = {
-                            scope.launch { drawerState.close() }
-                            selectedItem.value = item
-                        }
-                    ) {
-                        Text(
-                            item,
-                            fontSize = 22.sp,
-                            color = Color.White,
-                            modifier = Modifier.padding(vertical = 10.dp)
-                        )
-                    }
-                }
-            }
-        }
-    ) {
-        Box(Modifier.fillMaxSize()) {
-            Image(
-                painter = painterResource(id = R.drawable.back),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.back),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(top = 36.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 56.dp),
-                verticalArrangement = Arrangement.Top
+                Modifier
+                    .fillMaxWidth()
+                    .height(360.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
+                Text(
+                    text = "V P N",
+                    color = Color.White,
+                    fontSize = 40.sp,
+                    fontFamily = myFont,
+                    fontWeight = FontWeight.Light
+                )
                 Column(
                     Modifier
                         .fillMaxWidth()
-                        .height(360.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceBetween
+                        .padding(bottom = 120.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = "V P N",
-                        color = Color.White,
-                        fontSize = 40.sp,
+                        text = if (isConnected) "d i s c o n n e c t" else "c o n n e c t",
+                        color = Color.LightGray,
+                        fontSize = 32.sp,
                         fontFamily = myFont,
                         fontWeight = FontWeight.Light
                     )
-                    Column(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 120.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                }
+            }
+
+            Column(
+                Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    Modifier
+                        .size(220.dp)
+                        .drawBehind {
+                            val glowColor = Color(0xFF37FED9).copy(alpha = 0.99f)
+                            val radius = size.minDimension / 1.6f
+                            drawCircle(
+                                brush = Brush.radialGradient(
+                                    colors = listOf(glowColor, Color.Transparent),
+                                    center = center,
+                                    radius = radius
+                                ),
+                                radius = radius,
+                                center = center
+                            )
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    OutlinedButton(
+                        onClick = { isConnected = !isConnected },
+                        modifier = Modifier
+                            .size(290.dp)
+                            .clip(CircleShape),
+                        shape = CircleShape,
+                        colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.Black),
+                        border = BorderStroke(6.dp, color = Color(0xFF164C42)),
                     ) {
-                        Text(
-                            text = if (isConnected) "d i s c o n n e c t" else "c o n n e c t",
-                            color = Color.LightGray,
-                            fontSize = 32.sp,
-                            fontFamily = myFont,
-                            fontWeight = FontWeight.Light
+                        Image(
+                            painter = painterResource(id = R.drawable.conection),
+                            contentDescription = "Иконка коннект",
+                            modifier = Modifier.size(80.dp)
                         )
                     }
                 }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.End
+            ) {
+                IconButton(onClick = { isSheetOpen = true }) {
+                    Icon(
+                        Icons.Default.KeyboardArrowUp,
+                        contentDescription = "Меню",
+                        tint = Color.LightGray,
+                        modifier = Modifier.size(100.dp)
+                    )
+                }
+            }
+        }
+
+        if (isSheetOpen) {
+            ModalBottomSheet(
+                onDismissRequest = { isSheetOpen = false },
+                sheetState = sheetState,
+                containerColor = Color(0xFF164C42)
+            ) {
                 Column(
-                    Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Box(
-                        Modifier
-                            .size(220.dp)
-                            .drawBehind {
-                                val glowColor = Color(0xFF37FED9).copy(alpha = 0.99f)
-                                val radius = size.minDimension / 1.6f
-                                drawCircle(
-                                    brush = Brush.radialGradient(
-                                        colors = listOf(glowColor, Color.Transparent),
-                                        center = center,
-                                        radius = radius
-                                    ),
-                                    radius = radius,
-                                    center = center
-                                )
-                            },
-                        contentAlignment = Alignment.Center
-                    ) {
-                        OutlinedButton(
-                            onClick = { isConnected = !isConnected },
-                            modifier = Modifier
-                                .size(290.dp)
-                                .clip(CircleShape),
-                            shape = CircleShape,
-                            colors = ButtonDefaults.outlinedButtonColors(containerColor = Color.Black),
-                            border = BorderStroke(6.dp, color = Color(0xFF164C42)),
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.conection),
-                                contentDescription = "Иконка коннект",
-                                modifier = Modifier.size(80.dp)
-                            )
+                    Text("Меню", color = Color.White, fontSize = 28.sp,modifier = Modifier.padding(bottom = 30.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(horizontalArrangement = Arrangement.SpaceAround) {
+                        IconButton(onClick = {},  modifier = Modifier.padding(horizontal = 40.dp)) {
+                            Icon(Icons.Default.Settings, contentDescription = "Настройки", tint = Color.White,
+                                modifier = Modifier.size(90.dp))
+
                         }
+                        IconButton(onClick = {},  modifier = Modifier.padding(horizontal = 40.dp)) {
+                            Icon(Icons.Default.AccountCircle, contentDescription = "Акаунт", tint = Color.White,
+                                modifier = Modifier.size(90.dp))
+
+                        }
+                        IconButton(onClick = {}, modifier = Modifier.padding(horizontal = 40.dp)) {
+                            Icon(Icons.Default.Info, contentDescription = "Информация", tint = Color.White,
+                                modifier = Modifier.size(90.dp))
+
+                        }
+
                     }
+
                 }
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(
-                    selectedItem.value,
-                    fontSize = 28.sp,
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
-            }
-            IconButton(
-                onClick = {
-                    scope.launch {
-                        if (drawerState.isClosed) drawerState.open()
-                        else drawerState.close()
-                    }
-                },
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(16.dp)
-            ) {
-                Icon(
-                    Icons.Filled.Menu,
-                    contentDescription = "Кнопка меню",
-                    tint = Color.Gray
-                )
             }
         }
     }
 }
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
